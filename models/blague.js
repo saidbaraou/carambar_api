44f359caf095afe1eb3a4e1ae4169a0343a2ef03
Said <baraoudev@gmail.com>
2025-05-31 20:33:18 +0200

    feat: define blague model
    It has an id, a question and a reponse

---
+++ models/blague.js
@@ -0,0 +1,22 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../database/database.js');
+
+const Blague = sequelize.define('Blague', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey:true,
+    autoIncrement: true,
+  },
+  question: {
+    type: DataTypes:TEXT,
+    allowNull: false,
+    unique: true,
+  },
+  reponse: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  }
+})
+
+module.exports = Blague;
+       
