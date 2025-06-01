17d5fb8550677e874b74d4bba434060c9f8f43e8
Said <baraoudev@gmail.com>
2025-06-01 19:05:24 +0200

    feat: created logic of the app
    synchronized the db and start the server

--- app.js
+++ app.js
@@ -2,3 +2,20 @@ const express = require('express');
 const sequelize = require ('./database/database.sqlite')
 const blagueRoutes = require ('./routes/v1/blagueRoutes');
 const Blague = require('./models/Blague');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api/v1/blagues', blagueRoutes);
+
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('Base de données synchronisée.');
+    app.listen(PORT, () => {
+      console.log(`Serveur démarré sur le port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Erreur lors de la synchronisation de la base de données:', error);
+  });
