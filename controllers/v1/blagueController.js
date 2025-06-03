90f5fa065c35588663e7e234fb0c729a503ac2b3
Said <baraoudev@gmail.com>
2025-06-04 00:19:39 +0200

    fix: update files

--- controllers/v1/blagueController.js
+++ controllers/v1/blagueController.js
@@ -13,8 +13,26 @@ exports.addBlague = async (req, res) => {
   } catch (error) {
     if (error.name === 'SequelizeUniqueConstraintError') {
       return res.status(400).json({ error: 'Cette blague existe déjà.' });
-    }
+    } 
     res.status(500).json({ error: 'Erreur lors de l\'ajout de la blague.', details: error.message });
+  }
+}
+
+exports.getRandomBlague = async (req, res) => {
+  try {
+      const randomBlague = await blague.findOne({
+        order: [
+          sequelize.literal('RANDOM()')
+        ],
+        limit: 1
+    });
+    if (!randomBlague) {
+      return res.status(404).json({ error: 'Aucune blague trouvée.' });
+    }
+    res.status(200).json(randomBlague);
+    randomBlague.toJSON();
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur lors de la récupération d\'une blague aléatoire.', details: error.message });
   }
 }
 
@@ -40,20 +58,3 @@ exports.getBlagueById = async (req, res) => {
   }
 }
 
-exports.getRandomBlague = async (req, res) => {
-  try {
-      const randomBlague = await blague.findOne({
-        order: [
-          sequelize.literal('RANDOM()')
-        ],
-        limit: 1
-    });
-    if (!randomBlague) {
-      return res.status(404).json({ error: 'Aucune blague trouvée.' });
-    }
-    res.status(200).json(randomBlague);
-    randomBlague.toJSON();
-  } catch (error) {
-    res.status(500).json({ error: 'Erreur lors de la récupération d\'une blague aléatoire.', details: error.message });
-  }
-}
