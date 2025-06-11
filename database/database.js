e6f195009c8901980c34ac2539497cb8f8224dd9
Said <baraoudev@gmail.com>
2025-06-11 22:31:49 +0200

    fix: got rid of the portion of code that rewrote the dbpath everytime the server stopped

--- database/database.js
+++ database/database.js
@@ -7,9 +7,6 @@ const env = process.env.NODE_ENV || 'development';
 const dbConfig = config[env];
 
 const dbPath = path.join(__dirname, 'database.sqlite');
-// if (!fs.existsSync(dbPath)) {
-//   fs.writeFileSync(dbPath, '');
-// }
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
